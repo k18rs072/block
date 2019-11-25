1562ac4eba80aa75e3c0699d696663f89bf26d69
k18rs072 <k18rs072@st.kyusan-u.ac.jp>
2019-11-25 13:47:56 +0900

    10.3 keys.js　関連処理

---
+++ www/keys.js
@@ -0,0 +1,3 @@
+// This is a JavaScript file
+var appKey ="dc87a8bfb4711a82c6c6d67ae144ab8846f6f649f5cbebbd69879468671a0070";
+var clientKey ="7ee4da7bf929b2ad311208c0dc9497b607c25b742a099a30a56efa7c7b46406e";
